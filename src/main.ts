21e15f094accaf693f9a9a33d96b243465284821
ramunune <ramun.online@gmail.com>
2021-12-16 18:09:38 +0000

    Implement Favourites Component.

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'element-ui/lib/theme-chalk/index.css'
+import './assets/css/custom.css'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI, { locale })
--- src/views/Favourites.vue
+++ src/views/Favourites.vue
@@ -1,9 +1,33 @@
 <template>
-  <div>
-	Favourites Page
-  </div>
+	<el-row type="flex" justify="center">
+		<el-col :span="12">
+			<el-table :data="tableData" style="width: 100%">
+				<el-table-column prop="Title" label="" width="30">
+					<template slot-scope="scope">
+						<i :class="scope.row.star ? 'el-icon-star-on' : 'el-icon-star-off'" @click="toggleStar(scope.row)"></i>
+					</template>
+				</el-table-column>
+				<el-table-column prop="Title" label="Title"></el-table-column>
+				<el-table-column prop="Year" label="Year" width="180"></el-table-column>
+				<el-table-column prop="imdbID" label="ID" width="180"></el-table-column>
+			</el-table>
+		</el-col>
+	</el-row>
 </template>
-
 <script>
-export default {}
+import axios from 'axios'
+export default {
+	data () {
+		return {
+			tableData: JSON.parse(localStorage.getItem('favourites')) || []
+		}
+	},
+	methods: {
+		toggleStar (row) {
+			row.star = !row.star
+			this.tableData = this.tableData.filter(x => x.star)
+			localStorage.setItem('favourites', JSON.stringify(this.tableData))
+		}
+	}
+}
 </script>
