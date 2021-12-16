895316e6fd3b5fb99ac640c7200df5b5ed1ccf2d
ramunune <ramun.online@gmail.com>
2021-12-16 16:24:14 +0000

    Implement Search In Movies.

--- src/views/Movies.vue
+++ src/views/Movies.vue
@@ -1,21 +1,34 @@
 <template>
-	<el-row type="flex" justify="center">
-		<el-col :span="12">
-			<el-table :data="tableData" style="width: 100%">
-				<el-table-column prop="Title" label="Title"></el-table-column>
-				<el-table-column prop="Year" label="Year" width="180"></el-table-column>
-				<el-table-column prop="imdbID" label="ID" width="180"></el-table-column>
-			</el-table>
-		</el-col>
-	</el-row>
+	<div>
+		<el-row type="flex" justify="center" class="mb-20">
+			<el-col :span="12">
+				<el-input placeholder="Search Movie" v-model="search" @change="getMovies()" clearable></el-input>
+			</el-col>
+		</el-row>
+		<el-row type="flex" justify="center" v-loading="pending">
+			<el-col :span="12">
+				<el-table :data="tableData" style="width: 100%">
+					<el-table-column prop="Title" label="Title"></el-table-column>
+					<el-table-column prop="Year" label="Year" width="180"></el-table-column>
+					<el-table-column prop="imdbID" label="ID" width="180"></el-table-column>
+				</el-table>
+			</el-col>
+		</el-row>
+	</div>
 </template>
-
+<style>
+.mb-20 {
+	margin-bottom: 20px;
+}
+</style>
 <script>
 import axios from 'axios'
 export default {
 	data () {
 		return {
-			tableData: []
+			tableData: [],
+			pending: false,
+			search: ''
 		}
 	},
 	created () {
@@ -24,7 +37,11 @@ export default {
 	methods: {
 		async getMovies () {
 			try {
-				let {data} = await axios.get('https://jsonmock.hackerrank.com/api/movies')
+				this.pending = true
+				// https://jsonmock.hackerrank.com/api/movies/search/?Title=$%7Btitle%7D&page=$%7Bpage
+				let params = {}
+				if (this.search) params.Title = this.search
+				let {data} = await axios.get('https://jsonmock.hackerrank.com/api/movies/search', {params})
 				this.tableData = data.data
 			} catch (err) {
 				this.$message({
@@ -33,6 +50,8 @@ export default {
 					type: 'error'
 				})
 				throw new Error(err)
+			} finally {
+				this.pending = false
 			}
 		}
 	}
